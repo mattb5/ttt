e2f9a2be44544f553e8241cbf26842a78a28700b
mattb5 <mattbuffardi@gmail.com>
2016-05-30 09:19:41 -0400

    second commit - some event handlers working
    some event handler working, other not

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-// console.log("konichiwa bitches");
+'use strict';
 
-$( document ).ready(function() {
+	
+	$( document ).ready(function() {
+	
+	var counter = 1;
+	
+	if (counter % 2 == 0)
+		
+	{
+		$("#divOne").click(function(){
+	    	$(this).addClass("xBox");
+	    	counter++;
+	    	console.log(counter + " this is the counter ");
+	    	});
+
+
+		$("#divTwo").click(function(){
+	    	$(this).addClass("xBox");
+	    	counter++;
+	    	console.log(counter + " this is the counter ");
+	    	});
+	}
+	
+	
+	else
+	
+	
+	{		
+		$("#divOne").click(function(){
+	    	$(this).addClass("oBox");
+	    	counter++;
+	    	console.log(counter + " this is the counter ");
+	    	});
+	
+			
+		$("#divTwo").click(function(){
+	    	$(this).addClass("oBox");
+	    	counter++;
+	    	console.log(counter + " this is the counter ");
+	    	});
+	}		
+	
+	});	
+
 	
 
-	$("#divOne").click(function(){
-    	$(this).addClass("xBox");
-    	//$(this).hide();
-    	});
-	});
